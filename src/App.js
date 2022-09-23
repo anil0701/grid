d0053597b1ff38ef2403cad1860418e777791171
Kandula Anil <anil.kandula6@gmail.com>
2022-09-23 12:00:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Architecture Principles, Vision, And Requirements</h2>
+      <div className='a1'>
+      <p>Preliminary</p> 
+   
+      <div className='a2'>
+        <p>Architecture Vision</p>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
